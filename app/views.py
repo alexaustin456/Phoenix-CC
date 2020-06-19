3575564f39499151327e9c0d30cb929f0888e381
Alexander Austin <alexander.austin18@imperial.ac.uk>
2020-06-19 17:12:47 +0100

    Added all basic setups

--- app/views.py
+++ app/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+
 # Create your views here.
+def index(request):
+    return HttpResponseRedirect('home')
+
+
+def home(request):
+    context = {}
+    return render(request, 'app/home.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'app/contact.html', context)
+
+
+def results(request):
+    context = {}
+    return render(request, 'app/results.html', context)
+
--- phoenix/settings.py
+++ phoenix/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+if DEBUG:
+    ALLOWED_HOSTS.append('localhost')
+    ALLOWED_HOSTS.append('127.0.0.1')
+
 
 # Application definition
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'phoenix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
