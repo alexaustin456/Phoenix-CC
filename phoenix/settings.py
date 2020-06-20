c563cbddcf7e42bd85aeb921b1e944dc31e76a8e
Alexander Austin <alexander.austin18@imperial.ac.uk>
2020-06-20 14:54:41 +0100

    Added in template design

--- phoenix/settings.py
+++ phoenix/settings.py
@@ -124,3 +124,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
